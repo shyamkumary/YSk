0b4d35933b6258855be8304f1fb822ae744d1fcd
shyamkumary <96102481+shyamkumary@users.noreply.github.com>
2022-01-31 21:47:44 -0800

    Create AppTest.java

--- shyam/src/test/java/shyam/AppTest.java
+++ shyam/src/test/java/shyam/AppTest.java
@@ -1,20 +1,37 @@
-package shyam;
-
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
-}
+package JavaTpoint.JunitExamples;  
+import static org.junit.Assert.assertEquals;  
+import org.junit.Test;  
+public class TestJunitTestCaseExample {  
+           
+    JunitTestCaseExample obj = new JunitTestCaseExample();  
+     
+    @Test  
+    public void testAdd() {  
+        obj.add("Emma");  
+        obj.add("Ronan");  
+        obj.add("Antonio");  
+        obj.add("Paul");  
+        assertEquals("Adding 4 student to list", 4, obj.sizeOfStudent());  
+    }  
+      
+    @Test  
+    public void testSize() {  
+        obj.add("Emma");  
+        obj.add("Ronan");  
+        obj.add("Antonio");  
+        assertEquals("Checking size of List", 3, obj.sizeOfStudent());  
+    }  
+      
+    @Test  
+    public void testRemove() {  
+        obj.add("Antonio");  
+        obj.add("Paul");  
+        obj.remove("Paul");  
+        assertEquals("Removing 1 student from list", 1, obj.sizeOfStudent());  
+    }  
+      
+    @Test  
+    public void removeAll() {  
+        obj.removeAll();  
+    }  
+}  
